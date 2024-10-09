68c4d5e612b28cd0e60d4420270584c85b4ed831
FeridBiberoglu <biberoglu.feriddeniz@gmail.com>
2024-10-09 16:14:54 +0200

    split up ft_split mod

--- files/extra/helperfunc.c
+++ files/extra/helperfunc.c
@@ -113,17 +113,12 @@ char *ft_strndup(const char *s, size_t n)
     return result;
 }
 
-char **ft_split_mod(char const *s, char c)
+static int count_strings(char const *s, char c)
 {
     int strings = 0;
-    int i = 0;
     int len = 0;
     int quote_len = 0;
-    char **str;
-    const char *og_string = s;
 
-    if (s == NULL)
-        return (NULL);
     while (*s)
     {
         if (*s != c)
@@ -135,54 +130,81 @@ char **ft_split_mod(char const *s, char c)
         else
             s++;
     }
-    if (!(str = malloc((strings + 1) * sizeof(char *))))
-        return (NULL);
-    s = og_string;
+    return strings;
+}
+
+static void handle_quote(char s_k, char *outer_quote, char *inner_quote, char *token, int *j)
+{
+    if (*outer_quote == 0)
+        *outer_quote = s_k;
+    else if (s_k == *outer_quote)
+        *outer_quote = 0;
+    else if (*inner_quote == 0)
+    {
+        *inner_quote = s_k;
+        token[(*j)++] = s_k;
+    }
+    else if (s_k == *inner_quote)
+    {
+        *inner_quote = 0;
+        token[(*j)++] = s_k;
+    }
+}
+
+static char *extract_token(const char *s, int len)
+{
+    char *token = malloc(len + 1);
+    if (!token)
+        return NULL;
+
+    int j = 0, k = 0;
+    char outer_quote = 0, inner_quote = 0;
+
+    while (k < len)
+    {
+        if (is_quote(s[k]))
+            handle_quote(s[k], &outer_quote, &inner_quote, token, &j);
+        else
+            token[j++] = s[k];
+        k++;
+    }
+    token[j] = '\0';
+    return token;
+}
+
+static char **allocate_and_fill(char const *s, char c, int strings)
+{
+    char **str;
+    str = malloc((strings + 1) * sizeof(char *));
+    if (!str)
+        return NULL;
+    int i;
+    int len;
+    int quote_len;
+    
     i = 0;
+    len = 0;
+    quote_len = 0;
     while (i < strings)
     {
         while (*s == c)
             s++;
         len = get_token_length(s, &quote_len);
-        if (!(str[i] = malloc(len + 1)))
-        {
-            free_str_array(str, i);
-            return (NULL);
-        }
-        int j = 0;
-        int k = 0;
-        char outer_quote = 0;
-        char inner_quote = 0;
-        while (k < len)
-        {
-            if (is_quote(s[k]))
-            {
-                if (outer_quote == 0)
-                    outer_quote = s[k];
-                else if (s[k] == outer_quote)
-                    outer_quote = 0;
-                else if (inner_quote == 0)
-                {
-                    inner_quote = s[k];
-                    str[i][j++] = s[k];
-                }
-                else if (s[k] == inner_quote)
-                {
-                    inner_quote = 0;
-                    str[i][j++] = s[k];
-                }
-            }
-            else
-            {
-                str[i][j++] = s[k];
-            }
-            k++;
-        }
-        str[i][j] = '\0';
+        str[i] = extract_token(s, len);
+        if (!str[i])
+            return (free_str_array(str, i), NULL);
         s += len;
         i++;
     }
     str[i] = NULL;
-    print_str_array(str); 
-    return (str);
+    return str;
+}
+
+char **ft_split_mod(char const *s, char c)
+{
+    if (s == NULL)
+        return NULL;
+    int strings;
+    strings = count_strings(s, c);
+    return allocate_and_fill(s, c, strings);
 }
