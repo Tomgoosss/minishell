c151d60ccc081c6bb151b1b2382a1266ea1b3151
FeridBiberoglu <biberoglu.feriddeniz@gmail.com>
2024-10-14 16:45:20 +0200

    added shlvl

--- files/buildins/nodes.c
+++ files/buildins/nodes.c
@@ -38,16 +38,50 @@ void add_node(node_t **head, node_t *new)
 	temp->next = new;
 }
 
-// puts the env in a linked list
+char *create_shlvl(char *line)
+{
+	char *shlvl;
+	int temp;
+
+	shlvl = malloc(sizeof(char) * 4);
+	temp = line[6] - '0';
+	temp++;
+	if(temp > 1000)
+	{
+		ft_putstr_fd("minishell: warning: shell level (", 2);
+		ft_putnbr_fd(temp, 2);
+		ft_putstr_fd(") too high, resetting to 1\n", 2);
+		temp = 1;
+	}
+	shlvl[0] = temp + '0';
+	shlvl[1] = '\0';
+	return(shlvl);
+}
+
 void fill_nodes_env(t_env *var)
 {
-	int i;
+    int i;
+    char *shlvl;
+    char *temp;
 
-	i = 0;
-	while(var->env[i])
-	{
-		add_node(&var->head_env, make_node(var->env[i]));
-		add_node(&var->head_exp, make_node(var->env[i]));
-		i++;
-	}
+    i = 0;
+    while(var->env[i])
+    {
+        if(ft_strncmp(var->env[i], "SHLVL=", 6) == 0)
+        {
+            shlvl = create_shlvl(var->env[i]);;
+            temp = ft_strjoin("SHLVL=", shlvl);
+            if (!temp)
+                return (free(shlvl));
+            add_node(&var->head_env, make_node(temp));
+            free(temp);
+            free(shlvl);
+        }
+        else
+        {
+            add_node(&var->head_env, make_node(var->env[i]));
+            add_node(&var->head_exp, make_node(var->env[i]));
+        }
+        i++;
+    }
 }
